0682a1a4ce5f7675fcc02f3ae1dc0b9b31a8474f
Rongxiang Song <tinysong1226@gmail.com>
2020-04-24 18:01:58 +0800

    remove --cleanup flag in kube-proxy
     --cleanup  If true cleanup iptables and ipvs rules and exit.

--- cmd/kubeadm/app/phases/addons/proxy/manifests.go
+++ cmd/kubeadm/app/phases/addons/proxy/manifests.go
@@ -79,7 +79,6 @@ spec:
         - /usr/local/bin/kube-proxy
         - --config=/var/lib/kube-proxy/{{ .ProxyConfigMapKey }}
         - --hostname-override=$(NODE_NAME)
-        - --cleanup
         securityContext:
           privileged: true
         volumeMounts:
