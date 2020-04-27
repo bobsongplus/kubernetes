6fa83fcabc8102217c95c18791155a6803f57089
weiwei <weiwei@tenxcloud.com>
2020-04-27 20:12:43 +0800

    提交镜像构建脚本
    目前未实现自动化，后面可以整理下Makefile，实现一键构建所有镜像
    
    Signed-off-by: weiwei <weiwei@tenxcloud.com>

--- images/kubeadm/test.sh
+++ images/kubeadm/test.sh
@@ -7,8 +7,11 @@ export NETWORK_MODE=ipv4
 export SERVICE_DNS_DOMAIN=cluster.local
 export MASTERS=192.168.1.21,192.168.2.31
 export LOADBALANCES=192.168.1.21
-#export NETWORK_PLUGIN=weave
+#export NETWORK_PLUGIN=flannel
+export NETWORK_PLUGIN=weavenet
+export PROXY_MODE=iptables
 #./run.sh --address 192.168.1.52  --registry 192.168.1.52 --credential admin:vsvibpdwdssundxhhhljncnbcfieolczaeowuwggvoqkewsw  Init  http://192.168.1.103:48000/api/v2/cluster/new
 #./run.sh --address 192.168.1.52  --registry 192.168.1.52 --credential admin:vsvibpdwdssundxhhhljncnbcfieolczaeowuwggvoqkewsw  Init  http://192.168.1.103:48000/api/v2/cluster/new
-./run.sh  --registry 192.168.1.52 Init
+./run.sh  --registry 192.168.1.52 --credential admin:51c6735b118f11e9824312e2eae0bd5e  Init  http://192.168.4.175:8000/api/v2/cluster/new
 cat kubeadm-config.yaml
+cp kubeadm-config.tmpl.yaml kubeadm-config.yaml
